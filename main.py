7b17ef89b49466fd81709bd0a1a6aa3e6a133aa7
Mission Bit <missionbit@Missions-MBP.attlocal.net>
2020-09-30 16:38:16 -0700

    first step

---
+++ main.py
@@ -0,0 +1,15 @@
+def displayIntro():
+  #put an Introduction message to the users
+  print("Welcome to the nonprofit page!")
+
+def displayNonProfits():
+  #print all the non-profits to the screen numerically. For Example:
+#    1. World Central Kitchen
+#    2. Crisis Text Line
+#    3. Heart to Heart International
+
+    print (list = ["World Central Kitchen", "Crisis Text Line", "Heart to Heart International"]) 
+
+def main():
+    displayIntro()
+main()
